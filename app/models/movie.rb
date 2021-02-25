2a953c3cd8ef94e5193e12522eef2f1987362b40
EC2 Default User <ec2-user@ip-172-31-61-12.ec2.internal>
2021-02-25 23:40:20 +0000

    part1 test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+    def all_ratings
+        @ratings = ['G','PG','PG-13','R']
+        
+        return @ratings
+    end
 end
+
+h = Movie.new();
+puts h.all_ratings

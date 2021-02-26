48f79298bae13a5f7e2055f131be793fa2e0b3db
EC2 Default User <ec2-user@ip-172-31-61-12.ec2.internal>
2021-02-26 05:07:36 +0000

    part 3 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,6 @@ class Movie < ActiveRecord::Base
     
     scope :with_ratings, ->(selected_ratings) { where(rating:selected_ratings) }
     scope :all_ratings, ->{ distinct.order("rating").pluck("rating") }
-    
 end
 
 

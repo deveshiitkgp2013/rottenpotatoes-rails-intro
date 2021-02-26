d2f62b107e441a83d51c16de5e5b54db4b12f8b9
EC2 Default User <ec2-user@ip-172-31-61-12.ec2.internal>
2021-02-26 05:16:41 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,12 +13,7 @@ class MoviesController < ApplicationController
     @ratings_to_show = []
     @movies = Movie.all
     
-    if params[:sort].nil?
-      params[:sort] = session[:sort]
-    end
-    if params[:ratings].nil?
-      params[:ratings] = session[:ratings]
-    end
+    
      
     if params.include? :ratings
       selected_ratings = params[:ratings].keys

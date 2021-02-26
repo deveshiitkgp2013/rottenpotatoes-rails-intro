2826050579cf91659b8f31660f0810c9d80dfefd
EC2 Default User <ec2-user@ip-172-31-61-12.ec2.internal>
2021-02-26 05:30:42 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,12 @@ class MoviesController < ApplicationController
     
     logger.debug(session[:sort])
     
-     
+    if params[:sort].nil?
+      params[:sort] = session[:sort]
+    end
+    if params[:ratings].nil?
+      params[:ratings] = session[:ratings]
+    end 
     if params.include? :ratings
       selected_ratings = params[:ratings].keys
       if !selected_ratings.nil?
